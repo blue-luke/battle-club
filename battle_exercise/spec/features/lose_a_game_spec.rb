1241da721906ae3bf3aa000e7233ee74f27c6393
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-01 17:30:32 +0000

    Red feature test for starting a new game. Want a button that redirects to index page

--- battle_exercise/spec/features/lose_a_game_spec.rb
+++ battle_exercise/spec/features/lose_a_game_spec.rb
@@ -10,4 +10,16 @@ feature 'State next turn' do
 
     expect(page).to have_content "Well done, Jane, you have won!"
   end
+  scenario 'Link to starting new game is provided' do
+    sign_in_and_play
+
+    9.times do
+      click_button('Attack player 2')
+      click_button('Attack player 1')
+    end
+    click_button('Attack player 2')
+    click_button('New game')
+
+    expect(page).to have_content "Would you like to play a game?"
+  end
 end
