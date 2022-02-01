53ab67e6e451369fed7e025cf29921d3dc891687
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-01 15:19:04 +0000

    Changing what is displayed as next player in views

--- battle_exercise/lib/game.rb
+++ battle_exercise/lib/game.rb
@@ -7,15 +7,16 @@ class Game
   end
   def attack(player)
     player.get_attacked
+    switch_player
   end
   def players
     "#{@player1.name}, #{@player2.name}"
   end
   def switch_player
     if @next_player == @player1
-      @next_player == @player2
+      @next_player = @player2
     elsif @next_player == @player2
-      @next_player == @player1
+      @next_player = @player1
     end
   end
 end
