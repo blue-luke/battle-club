a2c6234cb579e9d760e7af229407d1b58bbdc4b8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-01 14:08:36 +0000

    Fully removed sessions, hp also handled by player class

--- battle_exercise/app.rb
+++ battle_exercise/app.rb
@@ -7,7 +7,7 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  enable :sessions
+  # enable :sessions
 
   # our routes would go here
 
@@ -19,33 +19,26 @@ class Battle < Sinatra::Base
     $player1 = Player.new(params[:player1])
     $player2 = Player.new(params[:player2])
 
-    p "Player 1 name below"
-    p $player1
-
-    # session[:player1] = params[:player1]
-    # session[:player2] = params[:player2]
-    session[:player1_hp] = 100
-    session[:player2_hp] = 100
     redirect '/names'
   end
 
   post '/deduct_player2_hp' do
-    session[:player2_hp] -= 10
+    $player2.get_attacked
+    # session[:player2_hp] -= 10
     redirect '/names'
   end
 
   post '/deduct_player1_hp' do
-    session[:player1_hp] -= 10
+    $player1.get_attacked
+    # session[:player1_hp] -= 10
     redirect '/names'
   end
 
   get '/names' do
     @player1 = $player1
     @player2 = $player2
-    # @player1 = session[:player1]
-    # @player2 = session[:player2]
-    @player1_hp = session[:player1_hp]
-    @player2_hp = session[:player2_hp]
+    # @player1_hp = session[:player1_hp]
+    # @player2_hp = session[:player2_hp]
     erb :play
   end
   # # Start the server if this file is executed directly (do not change the line below)
