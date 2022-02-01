06aec7c3f69226786f65adb7adc0262dc3c8ebe6
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-01 15:26:02 +0000

    Added test to check that player 2's turn comes next, not in red green refactor process though, RGF

--- battle_exercise/spec/features/state_next_turn_spec.rb
+++ battle_exercise/spec/features/state_next_turn_spec.rb
@@ -1,7 +1,13 @@
 feature 'State next turn' do
-  scenario 'Player 1 goes firest' do
+  scenario 'Player 1 goes first' do
     sign_in_and_play
 
-    expect(page).to have_content "It is Jane's go"
+    expect(page).to have_content "It is Jane's turn"
+  end
+  scenario 'Player 2 goes second' do
+    sign_in_and_play
+    click_button('Attack player 2')
+
+    expect(page).to have_content "It is John's turn"
   end
 end
