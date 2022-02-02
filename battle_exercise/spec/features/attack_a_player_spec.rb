22cd873f56d2cf34fdb77ce721e602847d52cc4a
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-02 12:34:53 +0000

    Added feature test for defending, is red

--- battle_exercise/spec/features/attack_a_player_spec.rb
+++ battle_exercise/spec/features/attack_a_player_spec.rb
@@ -1,5 +1,5 @@
 feature 'Attacking a player' do
-  scenario 'Geting confirmation' do
+  scenario 'Sucessfully attack' do
     allow_any_instance_of(Player).to receive(:calculate_damage).and_return(10)
     
     sign_in_and_play
@@ -7,5 +7,15 @@ feature 'Attacking a player' do
     click_button('Attack!')
 
     expect(page).to have_content 'John has 90 HP'
+  end
+  scenario 'Successfully defend' do
+    allow_any_instance_of(Player).to receive(:calculate_damage).and_return(10)
+    
+    sign_in_and_play
+
+    click_button('Defend!')
+    click_button('Attack!')
+
+    expect(page).to have_content 'Jane has 100 HP'
   end
 end
